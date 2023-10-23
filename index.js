64569fd3da4cc948e753d45e68b3ce9243d0c6b4
Mykhailo <skorpion.9337@gmail.com>
2023-10-23 16:15:54 +0300

    hw01

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let numberOne = 20;
+let numberTwo = 10
+
+// Addition
+console.log(numberOne + numberTwo);
+
+// Subtraction
+console.log(numberOne - numberTwo);
+
+// Multiplication
+console.log(numberOne * numberTwo);
+
+// Division
+console.log(numberOne / numberTwo);
+
+// Exponentiation
+console.log(numberOne ** numberTwo);
+
+// Square root
+console.log(Math.sqrt(numberOne));
+
+/*-----------------------------------*/
+
+let i = 0;
+
+// string
+console.log(typeof i.toString());
+
+// number
+console.log(typeof +i);
+
+// boolean
+console.log(typeof !i);
